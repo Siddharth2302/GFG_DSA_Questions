4a4f477fac2b0009e8c1679fdab5fdf3e8c270fe
Siddharth Rajesh <103493086+Siddharth2302@users.noreply.github.com>
2023-08-31 21:10:51 +0530

    Create Row_with_max_1s.cpp

---
+++ Row_with_max_1s.cpp
@@ -0,0 +1,22 @@
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    int i =0;
+	    int j = m-1;
+	    int ans=-1;
+	    while(j>=0 && i<n){
+	        if(arr[i][j]==1){
+	            j--;
+	            ans=i;
+	        }
+	        else{
+	            
+	            i++;
+	        }
+	        
+	    }
+	    return ans;
+	    
+	}
+
+};
